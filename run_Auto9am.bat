f3a08406edb090ad121a3b9b14ecb38cabf4602a
WIPARAT-17 <wiparatnimm@gmail.com>
2025-08-22 10:35:13 +0700

    Update run_Auto9am.bat

--- run_Auto9am.bat
+++ run_Auto9am.bat
@@ -1,2 +1,3 @@
 @echo off
-"C:\Users\1705w\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\1705w\OneDrive\Desktop\Auto9am\Auto9am.py"
+
+"C:\Users\YourUsername\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\YourUsername\OneDrive\Desktop\Auto9am\Auto9am.py"
